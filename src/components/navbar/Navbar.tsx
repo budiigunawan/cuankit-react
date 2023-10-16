2b7c9ea1bed38b51950a3071520a4d21538d312c
Budi Gunawan <budiindra.g@gmail.com>
2023-10-16 20:33:22 +0700

    [CUAN-001] add hamburger button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initFlowbite } from 'flowbite';
 
 function Navbar() {
+  useEffect(() => {
+    initFlowbite();
+  }, []);
+
   return (
     <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800'>
       <div className='container flex flex-wrap items-center justify-between mx-auto my-2'>
         <h2>Cuankit</h2>
+        <button
+          data-collapse-toggle='navbar-default'
+          type='button'
+          className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+          aria-controls='navbar-default'
+          aria-expanded='false'
+        >
+          <span className='sr-only'>Open main menu</span>
+          <svg
+            className='w-5 h-5'
+            aria-hidden='true'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 17 14'
+          >
+            <path
+              stroke='currentColor'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              d='M1 1h15M1 7h15M1 13h15'
+            />
+          </svg>
+        </button>
         <div
           id='navbar-default'
           className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
