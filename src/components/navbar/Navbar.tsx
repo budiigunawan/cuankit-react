8836459330f9c9348913d121830686c7892a315f
Budi Gunawan <budiindra.g@gmail.com>
2023-10-16 21:15:20 +0700

    [CUAN-001] add auth button component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavigationLinks from './NavigationLinks';
+import AuthButton from './AuthButton';
 
 function Navbar() {
   return (
@@ -7,20 +8,7 @@ function Navbar() {
       <div className='container flex flex-wrap items-center justify-between mx-auto my-2'>
         <h2>Cuankit</h2>
         <NavigationLinks />
-        <div className='hidden md:block md:order-2'>
-          <a
-            href='#'
-            className='px-8 py-3 mt-2 mr-2 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-sm md:px-8 hover:shadow'
-          >
-            Sign In
-          </a>
-          <a
-            href='#'
-            className='px-8 py-3 text-base font-medium text-white border border-transparent rounded-full bg-navy hover:bg-navy md:py-2 md:text-sm md:px-8 hover:shadow'
-          >
-            Sign Up
-          </a>
-        </div>
+        <AuthButton />
       </div>
     </nav>
   );
