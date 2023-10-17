02749489b63591ca170b2f38f1e4bbeb23065fb9
Budi Gunawan <budiindra.g@gmail.com>
2023-10-17 08:49:16 +0700

    [CUAN-002] add categories and category card component

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PageLayout from '../../components/layout/PageLayout';
 import Hero from '../../components/homepage/Hero';
+import Categories from '../../components/homepage/Categories';
 
 function Homepage() {
   return (
     <PageLayout>
       <Hero />
+      <Categories />
     </PageLayout>
   );
 }
