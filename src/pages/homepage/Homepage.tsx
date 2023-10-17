a512520911a45116443dbe1b14dbdd5dd00743b6
Budi Gunawan <budiindra.g@gmail.com>
2023-10-17 08:11:37 +0700

    [CUAN-002] add hero component

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PageLayout from '../../components/layout/PageLayout';
+import Hero from '../../components/homepage/Hero';
 
 function Homepage() {
   return (
     <PageLayout>
-      <div>Homepage</div>
+      <Hero />
     </PageLayout>
   );
 }
