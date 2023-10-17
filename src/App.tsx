dd06a0fb0316713a98049d6c52559517b55b06a1
Budi Gunawan <budiindra.g@gmail.com>
2023-10-17 10:54:53 +0700

    [CUAN-003] init register route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './assets/styles/index.css';
 import './assets/styles/fonts.css';
 import Homepage from './pages/homepage/Homepage';
+import Register from './pages/authentication/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='home' element={<Homepage />} />
-        <Route path='/' element={<Navigate to={'/home'} replace />} />
+        <Route path='register' element={<Register />} />
+        <Route path='/' element={<Navigate to={'home'} replace />} />
       </Routes>
     </BrowserRouter>
   );
