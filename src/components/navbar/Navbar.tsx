11fa24b2a96941d677957b45744f84e8a373878f
Budi Gunawan <budiindra.g@gmail.com>
2023-10-17 17:18:06 +0700

    [CUAN-005] adjust navbar logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import NavigationLinks from './NavigationLinks';
 import AuthButton from './AuthButton';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800'>
       <div className='container flex flex-wrap items-center justify-between mx-auto my-2'>
-        <h2>Cuankit</h2>
+        <Link to={'/home'}>
+          <h2 className='text-xl font-bold'>Cuankit</h2>
+        </Link>
         <NavigationLinks />
         <AuthButton />
       </div>
